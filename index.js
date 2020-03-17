5f937b9a5eb474b278506230d9235b6013ba3fe2
mattjagiello <mjagiello@gmail.com>
2020-03-17 14:34:57 -0500

    Done.

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ const tutorials = [
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'what is JSONP?',
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = (tutorials) => {
+//   return tutorials
+// }
+const array = [];
+
+function titleCased() {
+  tutorials.forEach((tutorial) => {
+    const splitStrings = tutorial.split(' ');
+    const capitalizedWords = splitStrings.map((letter) => letter.charAt(0).toUpperCase() + letter.slice(1));
+    const capitalizedStrings = capitalizedWords.join(' ');
+    array.push(capitalizedStrings);
+  });
+  return array;
 }
